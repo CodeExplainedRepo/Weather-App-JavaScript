f75ad9cf4302cfb75c7f2953a8ae46bf18269095
suryakantk94 <59741292+suryakantk94@users.noreply.github.com>
2020-08-08 13:31:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function showError(error){
 
 // GET WEATHER FROM API PROVIDER
 function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     
     fetch(api)
         .then(function(response){
